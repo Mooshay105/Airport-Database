83d43b8fec2f5c94ce24dc068d48a274e7c313bc
Malcolm Hauser <hauser.malcolm@gmail.com>
2024-09-03 14:39:54 +1000

    fix some flags

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,11 @@ function App() {
 		<div className="App">
 			<h1>Airports</h1>
 			<div className="cards">
-				{airports.map((airport) => (
-					<AirportCard key={airport.iata} airportName={airport.name} city={airport.city} country={airport.country} iataCode={airport.iata} icaoCode={airport.icao} imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/510px-Flag_of_the_United_Kingdom_%281-2%29.svg.png" />
-				))}
+				{airports.map((airport) => {
+					const imageUrl = imgURLs[airport.country];
+
+					return <AirportCard key={airport.iata} airportName={airport.name} city={airport.city} country={airport.country} iataCode={airport.iata} icaoCode={airport.icao} imgURL={imageUrl} />;
+				})}
 			</div>
 		</div>
 	);
