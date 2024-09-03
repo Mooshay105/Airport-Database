d0713c7e02e9ef0f9b8dd3149fbf564b65e85ccd
Malcolm Hauser <hauser.malcolm@gmail.com>
2024-09-03 12:15:09 +1000

    BIG DATA

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from "react-dom/client";
 import AirportCard from "./components/AirportCard";
 import "./assets/App.css";
+import imgURLs from "./assets/imgURLs.json";
+import airports from "./assets/airports.json";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Airports</h1>
 			<div className="cards">
-				<AirportCard airportName="London" city="London" country="United Kingdom" iataCode="LON" icaoCode="EGLL" imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/510px-Flag_of_the_United_Kingdom_%281-2%29.svg.png" />
-				<AirportCard airportName="New York" city="New York" country="United States" iataCode="JFK" icaoCode="KJFK" imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/510px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png" />
+				{airports.map((airport) => (
+					<AirportCard key={airport.iata} airportName={airport.name} city={airport.city} country={airport.country} iataCode={airport.iata} icaoCode={airport.icao} imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/510px-Flag_of_the_United_Kingdom_%281-2%29.svg.png" />
+				))}
 			</div>
 		</div>
 	);
