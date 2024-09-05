1467c2e9aeab13837a5d1b2b08944ea2aaeddd1e
Malcolm Hauser <hauser.malcolm@gmail.com>
2024-09-05 16:35:52 +1000

    Add A Notice

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import ReactDOM from "react-dom/client";
 import AirportCard from "./components/AirportCard";
+import NoticeBanner from "./components/NoticeBanner";
 import "./assets/App.css";
 import imgURLs from "./assets/imgURLs.json";
 import airports from "./assets/airports.json";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Airports</h1>
-			<div className="cards">
-				{airports.map((airport) => {
-					const imageUrl = imgURLs[airport.country as keyof typeof imgURLs];
+		<>
+			<NoticeBanner />
+			<div className="main">
+				<h1>Airports</h1>
+				<div className="cards">
+					{airports.map((airport) => {
+						const imageUrl = imgURLs[airport.country as keyof typeof imgURLs];
 
-					return <AirportCard key={airport.iata} airportName={airport.name} city={airport.city} country={airport.country} iataCode={airport.iata} icaoCode={airport.icao} imgURL={imageUrl} />;
-				})}
+						return <AirportCard key={airport.iata} airportName={airport.name} city={airport.city} country={airport.country} iataCode={airport.iata} icaoCode={airport.icao} imgURL={imageUrl} />;
+					})}
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
